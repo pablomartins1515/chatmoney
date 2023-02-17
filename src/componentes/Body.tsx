c931021371ef065ad99485e3fbea784dcd696a9d
Pablo Martins <pablomartins1515@hotmail.com>
2023-02-17 18:14:05 -0300

    Add files via upload

--- src/componentes/Body.tsx
+++ src/componentes/Body.tsx
@@ -10,15 +10,15 @@ export function Body() {
             <div >  
                 <span className="text-2xl justify-center flex flex-1 text-center">
                     <h1 className="">
-                        ChatGPT: Nova Inteligência artificial do <br/>
-                        Elon Musk está pagando até 50 dólares por dia
+                        ChatGPT: Nova Inteligência artificial<br/> do 
+                        Elon Musk está pagando <br/>até 50 dólares por dia
                     </h1>
                 </span>
             </div>
             
             <div className='justify-center flex flex-1 py-4 h-full '>
                 <div>
-                < ReactPlayer  url='https://crushmangabeiras2-0.wistia.com/medias/e47rp91edq' width='450px' height='250px'  />              
+                < ReactPlayer  url='https://crushmangabeiras2-0.wistia.com/medias/e47rp91edq' width='380px' height='220px'  />              
                 </div>
             </div>
             
--- src/componentes/Rodape.tsx
+++ src/componentes/Rodape.tsx
@@ -1,9 +1,9 @@
 
 export function Rodape() {
     return (
-    <div>      
-        <a href="">
-            <div className="py-5 mt-20 bg-gray-900 text-gray-50 font-semibold w-full flex items-center justify-center  boder-b boder-gray-60  hover:text-green-400 hover:opacity-50">            Política de Privacidade
+    <div className="py-12">      
+        <a>
+            <div className="py-4 mt-16 bg-gray-900 text-gray-50 font-semibold w-full flex items-center justify-center  boder-b boder-gray-60  hover:text-green-400 hover:opacity-50">            Política de Privacidade
             </div>
         </a>
     </div>
